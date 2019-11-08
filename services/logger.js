d7a91207e129dc7546e7b4e40595ab2c7c232f63
Emmanuel Amodu <emmanuelamodu@Emmanuels-MacBook-Pro.local>
2019-11-08 14:30:19 +0100

    initial commit

---
+++ services/logger.js
@@ -0,0 +1,37 @@
+const EventEmitter = require('events')
+
+/**
+ * Used for logging info to remote service
+ * const logger = require('./logger')
+ * logger.logMessage('logging this message')
+ * logger.addListener('onLogged', status => console.log(status))
+ * logger.addListener('onError', error => console.log(error))
+ */
+class Logger extends EventEmitter {
+    constructor() {
+        super();
+        this._url = 'https://'
+    }
+
+    async logMessage(message) {
+        try {
+            const logStatus = await this._log(message);
+            if (logStatus && logStatus.id) this.emit('onLogged', logStatus);
+            else this.emit('onError', {message: 'error'});
+        } catch(err) {
+            console.log(err)
+            this.emit('onError', null);
+        }
+    }
+
+    _log(message) {
+        return new Promise((resolve, reject) => {
+            // perform log operations here
+            const status = Math.floor(Math.random() * 10)
+            console.log('here', status)
+            status > 2 ? resolve({id: 1, logUrl: 'https://'}) : reject(null);
+        })
+    }
+}
+
+module.exports = new Logger();
